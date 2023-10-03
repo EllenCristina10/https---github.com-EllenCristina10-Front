fc89ffee1f8545e471e8a8417a74117dd9123483
Ellen Cristina <ellecristina42@gmail.com>
2023-10-03 12:59:12 -0300

    Criando classe de teste.
    Co-authored-by: PedroHSilva10 <PedroHSilva10@users.noreply.github.com>
    Co-authored-by: clebersonrch <clebersonrch@users.noreply.github.com>

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/student'); // Altere a URL para a página que deseja testar
+
+        $response->assertStatus(200);
+
+        $response->assertSee('<html>'); // Verifica se a tag HTML está presente
+        $response->assertSee('<head>'); // Verifica se a tag HEAD está presente
+        $response->assertSee('<body>'); // Verifica se a tag BODY está presente
+
+    }
+}
