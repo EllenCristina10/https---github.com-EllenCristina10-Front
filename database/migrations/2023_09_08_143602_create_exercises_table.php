2caeb3fbddb27e637560cf78a70a018656f8a2fd
Cleberson Rocha <clebersonrochaads@gmail.com>
2023-10-05 12:05:39 -0300

    Atualizando testes de browser utilizando dusk!
    Co-authored-by: Ellen Cristina <EllenCristina10@users.noreply.github.com>
    Co-authored-by: PedroHSilva10 <PedroHSilva10@users.noreply.github.com>

--- database/migrations/2023_09_08_143602_create_exercises_table.php
+++ database/migrations/2023_09_08_143602_create_exercises_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('listar_relatorios', function (Blueprint $table) {
+        Schema::create('exercises', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('listar_relatorios');
+        Schema::dropIfExists('exercises');
     }
 };
